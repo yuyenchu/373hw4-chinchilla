ba087b564e00910412956510dc8e2603edb32cbb
hoanguye <hdnguyen0305@gmail.com>
2018-10-26 17:36:13 -0700

    test#2

--- src/test/java/datastructures/TestAvlTree.java
+++ src/test/java/datastructures/TestAvlTree.java
@@ -50,11 +50,8 @@ public class TestAvlTree extends BaseTest {
         BST tree = new BST();
         tree.insert(10);
         tree.insert(5);
-        tree.insert(15);
-        tree.insert(2);
-        tree.insert(7);
         //tree.printSideways();
-        tree.getRoot().left.right.data = 1;
+        tree.getRoot().left.data = 12;
         //tree.printSideways();
         assertEquals(false, tree.isValidAVL());        
     }
